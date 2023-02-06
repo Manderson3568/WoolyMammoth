330a40e645a10547238d90b672e9af0b5773c90b
Michael Anderson <manderson3568@gmail.com>
2023-02-06 15:00:47 +1100

    intro image responsiveness added

--- src/components/blueButton/BlueButton.jsx
+++ src/components/blueButton/BlueButton.jsx
@@ -1,11 +1,27 @@
+import { forwardRef, useState, useEffect } from "react";
 import "./BlueButtonStyles.css";
-const BlueButton = ({ text, link }) => {
+const BlueButton = forwardRef(({ text, link }, ref) => {
+  const [visible, setVisible] = useState(false);
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting) {
+        setVisible(true);
+      }
+    });
+    if (ref) {
+      observer.observe(ref.current);
+    }
+  }, []);
   const arrow = ">";
   return (
-    <a href={`${link || "#"}`} className="blueButton">
+    <a
+      href={`${link || "#"}`}
+      className={`blueButton ${visible ? "visible" : ""}`}
+      ref={ref}
+    >
       <span className="buttonLeft">{text}</span>
       <span className="buttonRight">{arrow}</span>
     </a>
   );
-};
+});
 export default BlueButton;
--- src/components/introImage/IntroImage.jsx
+++ src/components/introImage/IntroImage.jsx
@@ -1,11 +1,14 @@
 import BlueButton from "../blueButton/BlueButton";
+import { useRef } from "react";
 import "./IntroImageStyles.css";
 const IntroImage = ({ buttonText, image, imageAlt }) => {
+  const button = useRef();
   return (
     <div className="introImage largeSection">
       <BlueButton
         text={buttonText}
         link="https://michael-anderson-portfolio.firebaseapp.com/"
+        ref={button}
       />
       <img src={image} alt={imageAlt} />
     </div>
